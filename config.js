6b67408eb5bf85a8ed0001f21974bff0d36e4d42
narakulayeshwanth <152397775+narakulayeshwanth@users.noreply.github.com>
2026-04-15 20:36:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-
 const config = {
-    // Change this to your Render backend URL in production
-    // e.g., "https://your-app-name.onrender.com"
-    API_BASE_URL: "http://localhost:5000/api",
-    SOCKET_URL: "http://localhost:5000"
+  API_BASE_URL: "https://online-examination-system-backend-1.onrender.com/api",
+  SOCKET_URL: "https://online-examination-system-backend-1.onrender.com"
 };
